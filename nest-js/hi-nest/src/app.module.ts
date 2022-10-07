fb1604c7fdfb899da313ea021196a69919331224
eminentecero <rapael860429@naver.com>
2022-10-07 23:54:37 +0900

    nest js 시작 전 세팅 완료

--- nest-js/hi-nest/src/app.module.ts
+++ nest-js/hi-nest/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 
+
+// 모든 모듈의 루트 모듈 역할
+// 데코레이터 : 클래스에 함수 기능을 추가할 수 있음.
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
